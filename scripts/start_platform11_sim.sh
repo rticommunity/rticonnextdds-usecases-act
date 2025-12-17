aad81761809708f36b51b534ed0256e8a5998aa5
David Greenberg <dgreenberg@rti.com>
2025-12-17 17:09:42 -0500

    clean up start scripts

---
+++ scripts/start_platform11_sim.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# (c) Copyright, Real-Time Innovations, 2025.  All rights reserved.
+# RTI grants Licensee a license to use, modify, compile, and create derivative
+# works of the software solely for use with RTI Connext DDS. Licensee may
+# redistribute copies of the software provided that all such copies are subject
+# to this license. The software is provided "as is", with no warranty of any
+# type, including any warranty for fitness for any purpose. RTI is under no
+# obligation to maintain or support the software. RTI shall not be liable for
+# any incidental or consequential damages arising out of the use or inability
+# to use the software.
+
+# Source Platform 11 parameters
+source ../params/platform_11_params.sh
+
+# Set verbosity
+# 0: dds.Verbosity.SILENT
+# 1: dds.Verbosity.EXCEPTION
+# 2: dds.Verbosity.WARNING
+# 3: dds.Verbosity.STATUS_ALL
+VERBOSITY=2
+
+################################################################################
+
+echo "
+-------------------------------- SIM CONFIGS: --------------------------------
+XML FILES:  $XML_FILES
+QOS_PROFILE:  $LAN_QOS_PROFILE
+DOMAIN_ID:  $DOMAIN_ID
+SOURCE:  $ROUTER_NAME
+DESTINATION:  $DESTINATION
+SESSION:  $SESSION_ID
+VERBOSITY:  $VERBOSITY
+-------------------------------- SIM CONFIGS: --------------------------------"
+
+# RUN Platform Simulator
+python3 ../node_sim/python/platform_sim.py --files ${XML_FILES} \
+                                 --qos_profile ${LAN_QOS_PROFILE} \
+                                 --domain_id ${DOMAIN_ID} \
+                                 --source ${ROUTER_NAME} \
+                                 --destination ${DESTINATION} \
+                                 --session ${SESSION_ID} \
+                                 --verbosity ${VERBOSITY}
